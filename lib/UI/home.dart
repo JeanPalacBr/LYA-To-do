4800e86f2e86e07d3693e8c4b10dcd2de31a7361
Jean Palacio Bravo <jeanpalacio@uninorte.edu.co>
2021-08-11 01:34:57 -0500

    FloatingActionMenu added

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'package:fab_menu_items/fab_menu_items.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,9 +11,74 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Todo"),
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade200,
+            title: Text("LYA To Do APP"),
+          ),
+          body: Center(
+            child: Text("LYA To Do APP"),
+          ),
+        ),
+        Fabmenuitems(
+          height: 160,
+          weith: 150,
+          animatedIcons: AnimatedIcons.add_event,
+          fabcolor: Colors.blue.shade200,
+          containercolor: Colors.white,
+          childrens: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.plus_one,
+                    size: 24,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                      child: Text(
+                    "Agregar una tarea",
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                  ))
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.add,
+                    size: 24,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                      child: Text(
+                    "Agregar varias tareas",
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                  ))
+                ],
+              ),
+            ),
+            Divider(),
+          ],
+        )
       ],
     );
   }
